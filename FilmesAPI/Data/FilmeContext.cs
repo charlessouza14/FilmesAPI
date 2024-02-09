8fd809370c57b9e7faa14db6411bd64fb2ac7e3f
Charles Souza <charles.souza.silva1995@gmail.com>
2024-02-08 22:55:22 -0300

    Criação classe endereço e todo seu escopo

--- FilmesAPI/Data/FilmeContext.cs
+++ FilmesAPI/Data/FilmeContext.cs
@@ -11,6 +11,7 @@ namespace FilmesAPI.Data
         }
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
 
     }
 }
